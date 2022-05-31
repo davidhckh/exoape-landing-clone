7fb9bca1e666c3409f54b837230b26d9d726c500
David <david.heckhoff@gmail.com>
2022-05-31 22:05:52 +0200

    :point_up_2: Added work items hover effect

--- src/Components/Home/Work/HomeWork.js
+++ src/Components/Home/Work/HomeWork.js
@@ -13,22 +13,56 @@ const HomeWork = () => {
     useEffect(() => {
         window.requestAnimationFrame(() => {
             //image 1
-            gsap.fromTo(image1.current, { y: 100 }, { y: -150, scrollTrigger: { trigger: image1.current, scrub: .9, }, })
+            gsap.fromTo(image1.current, { y: 100 }, { y: -130, scrollTrigger: { trigger: image1.current, scrub: .9, }, })
 
             //image 3
             gsap.fromTo(image3.current, { y: -50 }, { y: 100, scrollTrigger: { trigger: image3.current, scrub: .9, }, })
         })
     }, [])
 
+    const handleMouseEnter = (event) => {
+        //play video
+        const video = event.target.children[1]
+
+        if (video ? video.classList.contains('video') : false)
+            video.play()
+
+        //animation
+        const text = event.target.children[2].children[0]
+
+        if (text)
+            gsap.fromTo(text, { rotation: 10, opacity: 0, y: 15 }, { rotation: 0, y: 0, opacity: 1, duration: .7, ease: 'power4.easeOut' })
+    }
+
+    const handleMouseLeave = (event) => {
+        //pause video
+        const video = event.target.children[1]
+
+        if (video ? video.classList.contains('video') : false)
+            video.pause()
+
+        //animation
+        const text = event.target.children[2].children[0]
+
+        if (text)
+            gsap.fromTo(text, { rotation: 0, opacity: 1, y: 0 }, { rotation: -10, y: -15, opacity: 0, duration: .5, ease: 'power4.easeOut' })
+    }
+
     return (
         <section className="column content-width" id="home-work-section">
             <h1>Work</h1>
             <div className="row">
-                <div id="block-0" className="block">
-                    <img src="/home/work/ali-ali-hero.webp" height="3113" alt="Ottografie" loading="lazy"/>
-                    <video playsinline="" loop="loop" muted="muted" disablepictureinpicture="">
+                <div id="block-0" className="block" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <img src="/home/work/ali-ali-hero.webp" height="3113" alt="Ottografie" loading="lazy" />
+                    <video playsInline="" loop="loop" muted="muted" disablePictureInPicture="" className="video">
                         <source src="https://a.storyblok.com/f/133769/x/4d7b412c76/hover-ali.mp4" type="video/mp4" />
                     </video>
+                    <div className="text-wrapper">
+                        <p>
+                            <span><strong>Ali Ali - </strong></span>
+                            <span>Unique director's portfolio</span>
+                        </p>
+                    </div>
                 </div>
                 <div className="column" id="home-work-right-section">
                     <div className="row">
@@ -36,20 +70,44 @@ const HomeWork = () => {
                         <span>Featured Projects</span>
                     </div>
                     <p>Highlights of cases that we<br />passionately built with forward-<br />thinking clients and friends over<br />the years.</p>
-                    <div id="block-1" className="block" ref={image1}>
+                    <div id="block-1" className="block" ref={image1} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                         <img src="/home/work/ottografie-hero.webp" />
-                        <video playsinline="" loop="loop" muted="muted" disablepictureinpicture="">
+                        <video playsInline="" loop="loop" muted="muted" disablePictureInPicture="" className="video">
                             <source src="https://a.storyblok.com/f/133769/x/a02005ba43/hover-otto.mp4" type="video/mp4" />
                         </video>
+                        <div className="text-wrapper">
+                            <p>
+                                <span><strong>Ottografie - </strong></span>
+                                <span>Immersive photography portfolio</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="row">
-                <div id="block-3" className="block" ref={image3}>
+                <div id="block-3" className="block" ref={image3} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     <img src="/home/work/aebele-interiors-hero.webp" />
+                    <video playsInline="" loop="loop" muted="muted" disablePictureInPicture="" className="video">
+                        <source src="https://a.storyblok.com/f/133769/x/6c4b3b49c3/featured-work-aebele.mp4" type="video/mp4" />
+                    </video>
+                    <div className="text-wrapper">
+                        <p>
+                            <span><strong>Aebele Interiors - </strong></span>
+                            <span>Luxurious design experience</span>
+                        </p>
+                    </div>
                 </div>
-                <div id="block-2" className="block">
+                <div id="block-2" className="block" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     <img src="/home/work/rino-pelle-hero.webp" />
+                    <video playsInline="" loop="loop" muted="muted" disablePictureInPicture="" className="video">
+                        <source src="https://a.storyblok.com/f/133769/x/a183dd6f78/hover-rino.mp4" type="video/mp4" />
+                    </video>
+                    <div className="text-wrapper">
+                        <p>
+                            <span><strong>{'Rino & Pelle - '}</strong></span>
+                            <span>Unique director's portfolio</span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
